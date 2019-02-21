3bb75e097c09b2a01120280942e062002f625251
Jean Carlo <o.theface@gmail.com>
2019-02-20 21:32:26 -0300

    first

---
+++ index.js
@@ -0,0 +1,42 @@
+var express=require("express");
+var app= express();
+
+var sinesp = require('sinesp-nodejs');
+
+app.use('/placa/:q' , async (req, res) => {    
+    try {        
+        var placa = "EDQ5751";
+        console.log(req.params);
+        if (req.params.q != undefined) {placa = req.params.q;}
+        console.log(placa);
+
+        const dados = await sinesp.consultaPlaca(placa);
+        if (dados.codigoRetorno = '0') {
+            res.json({
+                "content" : {
+                    "type" : "text",
+                    "text" : "\nPlaca: " + dados.placa +
+                             "\nModelo: " + dados.modelo +
+                             "\nCor: " + dados.cor +
+                             "\nAno: " + dados.ano +
+                             "\nModelo: " + dados.anoModelo +
+                             "\nLocal: " + dados.municipio + "-" + dados.uf +
+                             "\nSituação: " + dados.situacao
+                }
+            })
+        } else {
+            res.json({
+                "message":"Erro ao processar consulta", 
+                "codigoRetorno": dados.codigoRetorno,
+                "mensagemRetorno" : dados.mensagemRetorno});
+        }
+
+    } catch (dados) {
+        console.log(dados);
+        res.json({"message":"Erro ao processar consulta", dados});
+    }
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
